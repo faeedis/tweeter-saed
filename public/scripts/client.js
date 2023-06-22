17104e14d324f4da9027c68e0e5ff5a3e76fde0f
saed <saedabdi3@gmail.comm>
2023-06-22 13:56:10 -0400

    update client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,12 +1,98 @@
+//function that escapes 
 $(document).ready(function() {
-  $('#tweet-text').on('input', function() {
-    const charCount = $(this).val().length;
-    const counter = $(this).siblings('div').children('.counter');
-    counter.text(140 - charCount);
-    if (charCount > 140) {
-      counter.addClass('invalid');
-    } else {
-      counter.removeClass('invalid');
+  const escape = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  
+  const createTweetElement = function(tweetData) {
+    const $tweet = $(`
+    <article class="tweet">
+    <header> 
+      <div class ="image">
+        <img src=" ${tweetData.user.avatars}" alt="avatar">
+        <p> ${tweetData.user.name} </p>
+      </div>
+      <p> ${tweetData.user.handle}</p>
+    </header>
+    <p class = "tweet-wrap">
+    ${escape(tweetData.content.text)}
+    </p>
+    <hr/>
+    <footer >
+    <p class ="date"> ${timeago.format(tweetData.created_at)}</p>
+    <div>
+    <i class="fa-solid fa-flag"></i>
+    <i class="fa-solid fa-retweet"></i>
+    <i class="fa-sharp fa-solid fa-heart"></i>
+    </div>
+    </footer >
+    </article>`
+
+    );
+    return $tweet;
+
+  };
+
+   // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+  const renderTweets = function(tweets) {
+    $("#tweet-container").empty();
+    for (const item of tweets) {
+      const $tweet = createTweetElement(item);
+      $(`#tweet-container`).prepend($tweet);
     }
+  };
+
+
+  const loadTweets = function() {
+    $.get("/tweets", function (data) {
+      renderTweets(data);
+    });
+
+  };
+  loadTweets();
+
+
+  // can escape character here
+  $("#my-form").on("submit", function(event) {
+    event.preventDefault();
+    $('#errorMessage').slideUp("slow");
+    const tweetLength = $('#tweet-text').val().length;
+    
+
+    console.log(tweetLength)
+    if (!tweetLength) {
+      $('#errorMessage').text("Tweet cannot be empty!");
+      $('#errorMessage').slideDown("slow");
+      return;
+
+    }
+
+    if (tweetLength >= 141) {
+      $('#errorMessage').text("Tweet can't be longer than 140 characters!");
+      $('#errorMessage').slideDown("slow");
+      return;
+
+    } 
+    const formData= ($(this).serialize());
+    $.ajax({
+      type: "POST",
+      url: '/tweets/',
+      data: formData,
+      success: function() {
+        $('#tweet-text').val('');
+        loadTweets();
+        $('.counter').val('140');
+      },
+    });
+
+    
+
   });
 });
+
+
